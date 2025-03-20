fa084514e57123302513eb96d75f69fcd1372168
Yuto0508 <yuto4139@outlook.jp>
2025-03-21 00:36:03 +0900

    最新バージョンによってエラーが生じていたがNode.js ver.16に直したら起動できたため修正履歴を残す

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon, getPokemon } from "./utils/pokemon.js";
 import Card from './components/Card/Card.js';
