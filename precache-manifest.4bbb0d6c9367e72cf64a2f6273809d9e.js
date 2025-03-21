5f53895d311d6278a241b90ca83c1e1f9f832837
Yuto0508 <yuto4139@outlook.jp>
2025-03-21 12:09:40 +0900

    Updates

---
+++ precache-manifest.4bbb0d6c9367e72cf64a2f6273809d9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b277e0892aeba463336a48d296729ed7",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "94d83bfce38ec5cfff10",
+    "url": "/pokemon-app/static/css/main.0ed6ffbf.chunk.css"
+  },
+  {
+    "revision": "85e164ac3c4b22e9d7c8",
+    "url": "/pokemon-app/static/js/2.fb752e6d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/pokemon-app/static/js/2.fb752e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d83bfce38ec5cfff10",
+    "url": "/pokemon-app/static/js/main.ffce5bb4.chunk.js"
+  },
+  {
+    "revision": "c53c8bc5e609b61faa17",
+    "url": "/pokemon-app/static/js/runtime-main.38e224ba.js"
+  }
+]);
